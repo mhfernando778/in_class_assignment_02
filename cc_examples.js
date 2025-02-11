a10a62cddc0bdaf090cf6357066a6983d99a7e44
Fernando <mhfernando778@gmail.com>
2025-02-11 16:05:37 -0500

    Task 1 - get products by category

---
+++ cc_examples.js
@@ -0,0 +1,13 @@
+// Task 1 //
+
+let products = [
+    {name: "Laptop", price: 1200, category: "Electronics"},
+    {name: "Phone", price: 800, category: "Electronics"},
+    {name: "Deask Chair", price: 150, category: "Furniture"}
+]
+
+function getProductsByCategory(products, category) {
+    return products.filter(product => product.category === category);
+}
+
+console.log(getProductsByCategory(products, "Electronics"));
