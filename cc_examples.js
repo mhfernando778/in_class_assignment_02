0dcfedf1fe456311da0c606b924d3b7b62921a8d
Fernando <mhfernando778@gmail.com>
2025-02-11 16:33:08 -0500

    Task 3 - calculated total revenue

--- cc_examples.js
+++ cc_examples.js
@@ -14,3 +14,23 @@ let products = [
 
 
 // Task 2 //
+// function applyDiscount(products, discountRate) {
+//     return products.map(product => ({
+//         ...product,
+//         price: product.price - (product.price * discountRate)
+//     }
+//     ))
+// }
+
+// console.log(applyDiscount(products, .1));
+
+
+// Task 3 //
+
+let sales = [250, 400, 150, 900, 1200];
+
+function calculateTotalRevenue(sales) {
+    return sales.reduce((total, sale) => total + sale, 0)
+}
+
+console.log(`Total Revenue 2024: $${calculateTotalRevenue(sales)}`)
